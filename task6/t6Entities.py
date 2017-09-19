2f13df83c082d3c76b1a6fbf4d37ac742f10b16d
Luke Maffey <maffeyl@vcu.edu>
2017-09-19 12:02:30 -0400

    Testing git...

---
+++ task6/t6Entities.py
@@ -0,0 +1,8 @@
+###############################
+# Programmer Name: Luke Maffey
+# Date: 9/19/17
+# Module Purpose: Class definitions for all TimeNorm entities - Intervals, Periods, Repeating-Intervals, and Operators.
+#################################
+
+class t6Entity:
+	def __init__(self)
