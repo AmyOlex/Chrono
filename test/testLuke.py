112b4949adafc01109de87980e814748de571fe7
Luke Maffey <maffeyl@vcu.edu>
2017-09-22 15:10:53 -0400

    Tested using SUTime with stopwords filtered

--- test/testLuke.py
+++ test/testLuke.py
@@ -7,10 +7,13 @@
 from task6 import utils
 from task6 import referenceToken
 import nltk
+import os
+import json
+from sutime import SUTime
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 
-file_path = "../../TrialData/TimeBank/wsj_0160/wsj_0160"
+file_path = "../../TrialData/TimeBank/wsj_0152/wsj_0152"
 stopwords_path = "../task6/stopwords_long"
 with open(stopwords_path) as raw:
     stopwords = raw.read().splitlines()
@@ -27,15 +30,20 @@ for w in words :
     if w not in stopwords :
         filtered_words.append(w) 
 # Just printing to make sure this works
-print("Filtered words: ", filtered_words)
+#print("Filtered words: ", filtered_words)
 print("Raw length: {}\nFiltered length: {}".format(len(words), len(filtered_words)))
 
 ## Converting to a reference Token list
+
+su_words = ''
 
 my_list = referenceToken.convertToRefTokens(t, span=s)
 my_list[1].setTemporal(1)
 print(str(len(my_list)))
 for tok in my_list:
     if tok.getText() not in stopwords:
-        print(tok)
+        su_words = su_words + tok.getText() + ' '
     
+jar_files = '/home/luke/NLP/python-sutime/jars/'
+sutime=SUTime(jars=jar_files, mark_time_ranges=True)
+print(json.dumps(sutime.parse(su_words),sort_keys=True,indent=4))
