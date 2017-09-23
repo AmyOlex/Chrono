ed23b0930e19c9ab92853380f9aec1c071df679d
Amy Olex <alolex@vcu.edu>
2017-09-23 10:25:00 -0400

    Added a test for the sutimeEntity class and import function.

--- test/test.py
+++ test/test.py
@@ -6,6 +6,8 @@
 
 from task6 import utils
 from task6 import referenceToken
+from task6 import sutimeEntity
+from task6 import sutime_wrapper
 
 r, t, s = utils.getWhitespaceSpans("/Users/alolex/Desktop/VCU_PhD_Work/CMSC516/project/TempEval-2013_PracticeData/wsj_0152/wsj_0152")
 
@@ -23,4 +25,13 @@ print(str(len(my_list)))
 for tok in my_list:
     print(tok)
     
+## Parsing with SUTime and converting to a list of sutimeEntity objects.
+path = "/Users/alolex/Desktop/VCU_PhD_Work/CMSC516/project/TempEval-2013_PracticeData/"
+#name = "wsj_0152/wsj_0152"
+name = "APW19980820.1428/APW19980820.1428"
+jars = "../task6/jars"
+json_str = sutime_wrapper.callSUTimeParse(path+name, jars)
+suList = sutimeEntity.getSutimeEntityList(sut_json=json_str)
+for s in suList:
+    print(s)
     
