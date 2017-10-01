2f1fb95709bae4be2cb22862de556744e01cfa77
Amy Olex <alolex@vcu.edu>
2017-10-01 18:23:28 -0400

    Added a method to convert a month type to an integer.

--- task6/utils.py
+++ task6/utils.py
@@ -179,7 +179,11 @@ def getNumberFromText(text):
 ####
 #END_MODULE
 ####      
-    
+  
+  
+def getMonthNumber(text):
+    month_dict = {'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8, 'September':9, 'October':10,'November':11, 'December':12}
+    return month_dict[text]
     
     
     
