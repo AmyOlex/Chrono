7ce70c065519485f404154ec93d57bb842b14952
nmortonVCU <mortonn@mymail.vcu.edu>
2017-10-10 16:50:38 -0400

    Update HeidTime_Wrapper.py

--- BaselineCode/HeidelTime/HeidTime_Wrapper.py
+++ BaselineCode/HeidelTime/HeidTime_Wrapper.py
@@ -1,3 +1,9 @@
+## Wrapper function for HeidelTime
+#
+# Programmer Name: Nicholas Morton
+#
+# Date: 10/10/2017
+
 import os
 import sys
 import HeidEntity
@@ -9,23 +15,19 @@ import json
 import nltk
 from nltk.tokenize import WhitespaceTokenizer
 
-def find_all(a_str,sub):
-    start = 0
-    while True:
-        start = a_str.find(sub, start)
-        if start == -1: return
-        yield start
-        start += len(sub) # use start += 1 to find overlapping matches
-
+## Takes in file path and returns a HeidTimeList
+# @author Nicholas Morton
+# @param file_path Path and file name of text file to be parsed.
+# @return A list of heidTime objects to be returned to the main program
 def runHeidTime(file_path): 
 
     with tempfile.TemporaryFile() as tempf:
-       command = "java -jar de.unihd.dbs.heideltime.standalone.jar " + file_path
+       command = "java -jar de.unihd.dbs.heideltime.standalone.jar " + file_path #Run HeidelTime command
        args=shlex.split(command)
        proc = subprocess.Popen(args, stdout=tempf)
        proc.wait()
        tempf.seek(0)
-       outputText=tempf.read()       
+       outputText=tempf.read()  #Read in HeidelTime output     
        heidList = []
        spanB = []
        spanE = []
@@ -33,14 +35,14 @@ def runHeidTime(file_path):
        file = open(file_path + ".dct", "r")
        docTime = file.read()
        id_counter = 0
-       strList, spanB, spanE = BuildTimex3List.getList(str(outputText))
+       strList, spanB, spanE = BuildTimex3List.getList(str(outputText)) #get list of TIMEX3 Entites
        file = open(file_path, "r")
        text = file.read()
        span_generator = WhitespaceTokenizer().span_tokenize(text)
        spans = [span for span in span_generator]
-       tokenized_text = WhitespaceTokenizer().tokenize(text)
+       tokenized_text = WhitespaceTokenizer().tokenize(text)  #create Span and token list to help discover proper spans for Heidlist
     
-       for i in range(len(strList)):
+       for i in range(len(strList)):                            #Build HeidTime list to be passed to the main entity
         hText = strList[i][strList[i].find('">')+2 : strList[i].find('</T')]
         hType = strList[i][strList[i].find('type="')+6 : strList[i].find('" value')]
         hValue = strList[i][strList[i].find('value="')+7 : strList[i].find('">')]
@@ -48,7 +50,7 @@ def runHeidTime(file_path):
         hStartSpan = 0
         hEndSpan = 0
 
-        for j in range(len(tokenized_text)):
+        for j in range(len(tokenized_text)):    #try to determine start and end span, needs improvement
             if hText in tokenized_text[j]:
                 hStartSpan, hEndSpan = spans[j]
                 tokenized_text = tokenized_text[i:]
