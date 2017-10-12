bc8bf7056a444f5352cf06dda9ffe94fb9007172
Amy Olex <alolex@vcu.edu>
2017-10-11 21:31:16 -0400

    updated folder names

--- runit.sh
+++ runit.sh
@@ -8,18 +8,18 @@
 
 ## Run the program and save results in ./results folder
 echo "Running T6 on input files....."
-python run_task6.py -i ./data/SemEval-Task6_TestData/ -o ./results/ -j ./jars/
+python run_task6.py -i ./data/SemEval-Task6_TrainData/ -o ./results/ -j ./jars/
 
 ## go to the anaforatools directory
 cd anaforatools
 
 ## run the evaluation script
 echo "Evaluating T6 Results..."
-python -m anafora.evaluate -r ../data/SemEval-Task6-Baseline -p ../results/ --exclude Event After Before Between Frequency Union Modifier Period This
+python -m anafora.evaluate -r ../data/SemEval-Task6-Gold -p ../results/ --exclude Event After Before Between Frequency Union Modifier Period This
 
 
 echo "Evaluating Baseline Results..."
-python -m anafora.evaluate -r ../data/SemEval-Task6-Baseline -p ../data/SemEval-Task6-Baseline-HeidelTime/ --exclude Event After Before Between Frequency Union Modifier Period This --overlap
+python -m anafora.evaluate -r ../data/SemEval-Task6-Gold -p ../data/SemEval-Task6-Baseline-HeidelTime/ --exclude Event After Before Between Frequency Union Modifier Period This --overlap
 
 ## change back to main directory
 cd ../
