3c6eae026fc37caf8107822f234809530c31ebf9
Amy Olex <alolex@vcu.edu>
2017-11-11 12:02:19 -0500

    added file for parsing out Periods and Calendar Intervals from the gold standard XML files.

---
+++ task6/gold_standard_utils.py
@@ -0,0 +1,61 @@
+## Gold Standard Processing
+## The functions in this file provide all the parsing for processing the gold standard files for ML methods.
+#
+# Programmer Name: Amy Olex
+#
+# Date: 11/11/17
+
+import xmltodict
+import csv
+import os
+import sys
+
+
+## Iterates through all the gold standard XML files to parse out specified entities to save them to a tab delimited text file for ML method reference.  This function only need to be run once to generate all the files.
+# @author Amy Olex
+# @param path The path to the main folder where all the gold standard files are stored. Assumption is one folder per gold standard with only one xml file present in each folder.
+def parseGoldEntities(path):
+    # walk through all the folders to collect the xml file names
+    dirs = []
+    files = []
+    for root, dirs, files in os.walk(path, topdown=True):
+        for subdir in dirs:
+            for file in os.listdir(root+subdir):
+                fname, ext = os.path.splitext(file)
+                if ext == '.xml':
+                    ## parse out the Calendar-Interval and Period entities for each file
+                    extractPeriodCalInterval(root+subdir, file)
+                    ## can add additional gold standard parsing here...
+
+
+## Parses out the "Period" and "Calendar-Interval" entities to save them to a tab delimited text file for ML method reference.  This function only need to be run once to generate all the files.
+# @author Amy Olex
+# @param path The path to the gold standard file to be parsed.  
+def extractPeriodCalInterval(path, file):   
+    with open(path+"/"+file) as fd:
+        doc = xmltodict.parse(fd.read())
+	
+    entities = doc['data']['annotations']['entity']
+
+    my_golds = []
+    for entity in entities:
+        etype = entity['type']
+        if etype in ['Calendar-Interval', 'Period']:
+            start,end = entity['span'].split(",")
+            value = entity['properties']['Type']
+            my_golds.append({'type': etype, 'start': start, 'end':end, 'value': value})
+    if len(my_golds) > 0:	
+        keys = my_golds[0].keys()
+        with open((path+'/period-interval.gold.csv'), 'w') as output_file:
+            dict_writer = csv.DictWriter(output_file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(my_golds)
+    else:
+        open((path+'/period-interval.gold.csv'), 'w')
+
+############
+
+path='/Users/alolex/Desktop/VCU_PhD_Work/CMSC516/project/CMSC516-SemEval2018-Task6/data/SemEval-Task6-Gold/'
+
+print(path)
+parseGoldEntities(path)
