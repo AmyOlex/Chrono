d85f86764e26db994e05502ec80d7c8f0a97bf32
Luke Maffey <maffeyl@vcu.edu>
2017-11-30 16:21:39 -0500

    ChronoNN takes into account

--- ChronoNN/ChronoKeras.py
+++ ChronoNN/ChronoKeras.py
@@ -43,7 +43,7 @@ def keras_evaluate(model,test_data,test_labels):
 def keras_classify(model,predict_data):
     X = []
     X.append(list(predict_data))
-    X.append(list(predict_data))
+    # X.append(list(predict_data))
     # print("Predicting on {}".format(X))
     prediction = model.predict(X,verbose=1)
     print("The prediction is: {}".format(np.round(prediction[0])))
