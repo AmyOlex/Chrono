8adda42bb4185e059c073d1e44cf1f0f0c19a6eb
maffeyl <31329104+maffeyl@users.noreply.github.com>
2017-12-04 20:51:32 -0500

    Update ChronoKeras.py

--- ChronoNN/ChronoKeras.py
+++ ChronoNN/ChronoKeras.py
@@ -1,6 +1,8 @@
+## Uses Keras to build, train, a classify with a neural network
+# @author Luke Maffey
+
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-# from gensim.models import word2vec
 from gensim.models.keyedvectors import KeyedVectors
 
 import numpy as np
@@ -9,15 +11,19 @@ import string
 import math
 import random
 
-np.random.seed(123)
-
+## Builds and trains the neural network with the given training data
+# @param train_data A CSV with the training data
+# @param train_labels A CSV with the training labels
+# @return The neural network classifier, pass this to keras_classify and keras_evaluate
 def build_model(train_data, train_labels):
+    # Import the given data
     dataset = np.loadtxt(train_data, delimiter=",",skiprows=1)
     labels = np.loadtxt(train_labels, delimiter=",")
     size = len(dataset[0,:])
     print("Size: {}".format(size))
     X = dataset[:, 0:size]
     Y = labels[:]
+    
     # Build keras NN
     model = Sequential()
     model.add(Dense(size, input_shape=(size, ), activation='relu'))
@@ -31,18 +37,27 @@ def build_model(train_data, train_labels):
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     
-    model.fit(X, Y, epochs=3, batch_size=10)
+    # Train the NN, change epochs to train longer or shorter
+    model.fit(X, Y, epochs=5, batch_size=10)
     return model
 
+## Evaluate the given model on a test set
+# @param model The model to be evaluated
+# @param test_data A CSV with the test data
+# @param train_labels A CSV with the test labels
+# @return Prints and returns the accuracy of the model on the test data
 def keras_evaluate(model,test_data,test_labels):
     scores = model.evaluate(test_data, test_labels)
     print("Test data accuracy: {}".format(scores[1]*100))
     return scores
 
-
+## Classfy a single sample
+# @param model The model to be used
+# @param predcit_data A numpy array with the sample to be classified
+# @return A 0 or 1 for which class was predicted
 def keras_classify(model,predict_data):
+    # Keras wants a list of numpy arrays
     X = []
-    X.append(list(predict_data))
     X.append(list(predict_data))
     #print("Predicting on {}".format(X))
     prediction = model.predict(X,verbose=1)
