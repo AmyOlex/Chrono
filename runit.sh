aaf4657c3c1552549477a5573183cdce8a6d35f1
Amy Olex <alolex@vcu.edu>
2017-12-05 21:18:19 -0500

    added command line option for ML method.

--- runit.sh
+++ runit.sh
@@ -4,12 +4,14 @@
 # Description: 	This bash script executes the SemEval 2018 Task6 Time Normalizations project, then evaluates the results.
 # 		You must be in the top level of the CMSC516-SemEval2018-Task6 folder to run this project.
 #
-# Usage: >> source runit.sh
+# Usage: >> source runit.sh [NB | NN | DT]
+
+ML=$1
 
 ## Run the program and save results in ./results folder
 echo "Running T6 on input files....."
 #python run_task6.py -i ./testdata/train/ -r ./testdata/gold/ -o ./testresults/ -t 0 -m NB
-python run_task6.py -i ./data/SemEval-Task6-Test/ -r ./data/SemEval-Task6-TestGold/ -o ./resultsTest/ -t 0 -m NB
+python run_task6.py -i ./data/SemEval-Task6-Test/ -r ./data/SemEval-Task6-TestGold/ -o ./resultsTest/ -t 0 -m $ML
 ## go to the anaforatools directory
 cd anaforatools
 
