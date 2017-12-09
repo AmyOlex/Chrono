418c0631097fed453694afb73552930d5c6e7e72
Amy Olex <alolex@vcu.edu>
2017-12-08 21:57:08 -0500

    minor updates

--- runit.sh
+++ runit.sh
@@ -9,7 +9,7 @@
 ML=$1
 
 ## Run the program and save results in ./results folder
-echo "Running Chrono on input files....."
+echo "Running Chrono on Test files....."
 python run_chrono.py -i ./data/SemEval-Task6-Test/ -r ./data/SemEval-Task6-TestGold/ -o ./resultsTest/ -m $ML
 
 ## Run the following if you want to create a new training data matrix
@@ -19,14 +19,14 @@ python run_chrono.py -i ./data/SemEval-Task6-Test/ -r ./data/SemEval-Task6-TestG
 cd anaforatools
 
 ## run the evaluation script
-echo "Evaluating Chrono Results..."
+echo "Evaluating Chrono Test Results..."
 python -m anafora.evaluate -r ../data/SemEval-Task6-TestGold/ -p ../resultsTest/ --exclude Event After Before Between Frequency Union Modifier This Intersection NthFromStart
 
 ## go to baselinecode Directory
 cd ../
 
 ## run GUTime Baseline Code
-echo "Running GUTime on input files..."
+echo "Running GUTime on Test files..."
 cd BaselineCode/GUTime
 python run_baseline_code.py -i ../../data/SemEval-Task6_GUTime-TestData/ -o ../../baseline_resultsTest/
 
@@ -34,7 +34,7 @@ python run_baseline_code.py -i ../../data/SemEval-Task6_GUTime-TestData/ -o ../.
 cd ../../
 cd anaforatools
 
-#echo "Evaluating Baseline Results..."
+echo "Evaluating Baseline Test Results..."
 python -m anafora.evaluate -r ../data/SemEval-Task6-TestGold -p ../baseline_resultsTest/ --exclude Event After Before Between Frequency Union Modifier This Intersection NthFromStart --overlap
 
 ## change back to main directory
