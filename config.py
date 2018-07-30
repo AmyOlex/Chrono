6c08dd0c4a298425c0347ee75fb4866ef7c31704
maffeyl <31329104+maffeyl@users.noreply.github.com>
2018-07-30 17:28:14 -0400

    Have to import all the builds into config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from Chrono.TimePhraseToChrono import *
+
 # Dictionary is a command line option read in from a folder where each filename is an entity type and each file
 # contains a list of words associated with that entity
 DICTIONARY = {}
