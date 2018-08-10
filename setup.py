bc5dee59ed4bca3250258fbbe588e7d772ad0c8a
maffeyl <maffeyl@vcu.edu>
2018-08-10 10:57:17 -0400

    Updates to installation setup

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['nltk', 'python-dateutil', 'numpy', 'sklearn', 'keras', 'tensorflow'],
+    install_requires=['nltk', 'python-dateutil', 'numpy', 'scikit-learn', 'keras', 'tensorflow'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
