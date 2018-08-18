ecb0f6659bca37e9d68b04c35c3496b06c445f92
maffeyl <31329104+maffeyl@users.noreply.github.com>
2018-08-18 11:43:25 -0400

    Created token counter

---
+++ Chrono/utils/TokenCounter.py
@@ -0,0 +1,110 @@
+# Copyright (c) 2018
+# Amy L. Olex, Virginia Commonwealth University
+# alolex at vcu.edu
+#
+# Luke Maffey, Virginia Commonwealth University
+# maffeyl at vcu.edu
+#
+# Nicholas Morton,  Virginia Commonwealth University
+# nmorton at vcu.edu
+#
+# Bridget T. McInnes, Virginia Commonwealth University
+# btmcinnes at vcu.edu
+#
+# This file is part of Chrono
+#
+# Chrono is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# Chrono is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Chrono; if not, write to
+#
+# The Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330,
+# Boston, MA  02111-1307, USA.
+import argparse
+from pathlib import Path
+from Chrono import utils, referenceToken
+
+
+def count_total_tokens(filename, outfile):
+    text, tokens, spans, tags, sents = utils.getWhitespaceTokens(str(filename))
+    with outfile.open('a+') as f:
+        f.write("Total number of tokens: ", len(tokens))
+
+
+def count_temporal_tokens(filename, outfile):
+    text, tokens, spans, tags, sents = utils.getWhitespaceTokens(str(filename))
+    my_refToks = referenceToken.convertToRefTokens(tok_list=tokens, span=spans, pos=tags, sent_boundaries=sents)
+    chroList = utils.markTemporal(my_refToks)
+    doctime = utils.getDocTime(str(filename) + ".dct")
+    tempPhrases = utils.getTemporalPhrases(chroList, doctime)
+    with outfile.open('a+') as f:
+        f.write("Total: ", len(tempPhrases))
+        for p in tempPhrases:
+            f.write(p)
+
+
+# http://ominian.com/2016/03/29/os-walk-for-pathlib-path/
+def path_walk(top, topdown=False, followlinks=False):
+    """
+         See Python docs for os.walk, exact same behavior but it yields Path() instances instead
+    """
+    names = list(top.iterdir())
+
+    dirs = (node for node in names if node.is_dir() is True)
+    nondirs = (node for node in names if node.is_dir() is False)
+
+    if topdown:
+        yield top, dirs, nondirs
+
+    for name in dirs:
+        if followlinks or name.is_symlink() is False:
+            for x in path_walk(name, topdown, followlinks):
+                yield x
+
+    if topdown is not True:
+        yield top, dirs, nondirs
+
+
+if __name__ == "__main__":
+    # Parse input arguments
+    parser = argparse.ArgumentParser(
+        description='Parse a directory of files to count the number of tokens')
+    parser.add_argument('-i', metavar='inputdir', type=str, help='path to the input directory.', required=True)
+    parser.add_argument('-x', metavar='fileExt', type=str,
+                        help='input file extension if exists. Default is an empty string', required=False, default="")
+    parser.add_argument('-o', metavar='outputFilePrefix', type=str,
+                        help='The output file name prefix with path if needed.', required=True)
+
+    args = parser.parse_args()
+    # Now we can access each argument as args.i, args.o, args.r
+
+    # get list of folder names in the input directory
+    indirs = []
+    infiles = []
+    outfiles = []
+    outdirs = []
+    goldfiles = []
+    preout = Path(args.o)
+    prein = Path(args.i)
+    for root, dirs, files in path_walk(args.i, topdown=True):
+        for name in dirs:
+            indirs.append(root.joinpath(name))
+            infiles.append(root.joinpath(name, name))
+            outfiles.append(preout.joinpath(name, name))
+            outdirs.append(preout.joinpath(name))
+            if not prein.joinpath(name).exists():
+                print(str(prein.joinpath(name)) + " DOES NOT EXISTS")
+                break
+    for f in infiles:
+        count_total_tokens(f, f.name + ".count")
+    print("Completed creating ML training matrix files.")
+
