c73d26364f6ce801c3b5766d664098bf6a36a943
maffeyl <31329104+maffeyl@users.noreply.github.com>
2019-02-02 13:10:55 -0500

    Attempting to fix dictionary location

--- Chrono/ChronoUtils/initialize_chrono.py
+++ Chrono/ChronoUtils/initialize_chrono.py
@@ -61,10 +61,10 @@ def setup_ML(ml_input, ml_model, train_data, train_labels):
 
 
 def initialize(in_dictionary="dictionary"):
-    dict_path = pkg_resources.resource_filename('data', '/')
+    dict_path = pkg_resources.resource_filename('dictionary', 'dictionary/')
     # Read in the word lists for each entity
-    path = Path(dict_path).parent.joinpath(Path(in_dictionary))
-    if Path(dict_path).exists():
+    path = Path(dict_path)
+    if path.exists():
         for root, dirs, files in path_walk(path, topdown=True):
             for file in files:
                 with file.open() as f:
--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='3.0.0',
+    version='3.0.1',
 
     description='Chrono is a hybrid rule-based and machine learning system that identifies temporal expressions in text and normalizes them into the Semantically Compositional Annotations for Temporal Expressions (SCATE) schema developed by Bethard and Parker. Chrono has emerged as the top performing system for SemEval 2018 Task 6: Parsing Time Normalizations.',
     long_description=long_description,
