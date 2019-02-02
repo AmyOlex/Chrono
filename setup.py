6bafcf6720223c006f25aed7d67295800a8a6071
maffeyl <31329104+maffeyl@users.noreply.github.com>
2019-02-02 18:58:38 -0500

    Changed name in setup to ChronoNLP

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='Chrono',
+    name='ChronoNLP',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
