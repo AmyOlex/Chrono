ef553f5ee38558ff4fc39aed6c0b26ce482cd810
Amy Olex <alolex@vcu.edu>
2020-05-17 09:27:36 -0400

    added a new method to return the chrono entity values for ISO conversion

--- Chrono/utils.py
+++ Chrono/utils.py
@@ -738,4 +738,91 @@ def getEntityTypes(chrono_list):
             
     return(year,month,day,hour,minute,second,daypart,dayweek,interval,period,nth,nxt,this,tz,ampm,modifier,last,entity_count)
     
+
+## Takes in list of ChronoEntities and returns the values, blank string if no value
+# @author Amy Olex
+# @param list of ChronoEntities
+# @return List of ChronoEntitie values
+def getEntityValues(chrono_list):
+    year = ""
+    month = ""
+    day = ""
+    hour = ""
+    minute = ""
+    second = ""
+    daypart = ""
+    dayweek = ""
+    interval = ""
+    period = ""
+    nth = ""
+    nxt = ""
+    this = ""
+    tz = ""
+    ampm = ""
+    modifier = ""
+    last = ""
+   
+    ## loop through all entities and pull out the approriate IDs
+    for e in range(0,len(chrono_list)):
+        #print(chrono_list[e].get_id())
+        e_type = chrono_list[e].get_type()
+        #print("E-type: " + e_type)
+        
+        if e_type == "Two-Digit-Year" or e_type == "Year":
+            year = chrono_list[e].get_value()
+
+            # print("YEAR VALUE: " + str(chrono_list[e].get_value()))
+        elif e_type == "Month-Of-Year":
+            # print("FOUND Month")
+            month = chrono_list[e].get_value()
+            
+        elif e_type == "Day-Of-Month":
+            day = chrono_list[e].get_value()
+           
+        elif e_type == "Hour-Of-Day":
+            hour = chrono_list[e].get_value()
+            
+        elif e_type == "Minute-Of-Hour":
+            minute = chrono_list[e].get_value()
+            
+        elif e_type == "Second-Of-Minute":
+            second = chrono_list[e].get_value()
+            
+        elif e_type == "Part-Of-Day":
+            daypart = chrono_list[e].get_value()
+            
+        elif e_type == "Day-Of-Week":
+            dayweek = chrono_list[e].get_value()
+            
+        elif e_type == "Calendar-Interval":
+            interval = chrono_list[e].get_value()
+            
+        elif e_type == "Period":
+            period = chrono_list[e].get_value()
+            
+        elif e_type == "NthFromStart":
+            nth = chrono_list[e].get_value()
+            
+        elif e_type == "Next":
+            nxt = chrono_list[e].get_value()
+            
+        elif e_type == "This":
+            this = chrono_list[e].get_value()
+            
+        
+        elif e_type == "Time-Zone":
+            tz = chrono_list[e].get_value()
+            
+        elif e_type == "AMPM-Of-Day":
+            ampm = chrono_list[e].get_value()
+            
+        elif e_type == "Modifier":
+            modifier = chrono_list[e].get_value()
+            
+        elif e_type == "Last":
+            last = chrono_list[e].get_value()
+            
+            
+    return(year,month,day,hour,minute,second,daypart,dayweek,interval,period,nth,nxt,this,tz,ampm,modifier,last)
     
+       
